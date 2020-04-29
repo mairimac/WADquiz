27c9d006156814e10e3eb785e62af364d053024a
mairimac <mairi@base66.co.uk>
2020-04-29 11:19:56 +0100

    Updates

---
+++ precache-manifest.57e3587209722b3ceb935a80004deaba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaf4b7a63964a00387e6bd1ee23a949",
+    "url": "/mairimac/WADquiz/index.html"
+  },
+  {
+    "revision": "07f301d885b35897f8c0",
+    "url": "/mairimac/WADquiz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "633969f46944b04c11d8",
+    "url": "/mairimac/WADquiz/static/js/2.889ead92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mairimac/WADquiz/static/js/2.889ead92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f301d885b35897f8c0",
+    "url": "/mairimac/WADquiz/static/js/main.f6903629.chunk.js"
+  },
+  {
+    "revision": "1efeae727f87482d033b",
+    "url": "/mairimac/WADquiz/static/js/runtime-main.d05b2e44.js"
+  }
+]);
